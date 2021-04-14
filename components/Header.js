dba5ca4586b86ddccd80c62783445980f09e51fa
unknown <john.ol1992@gmail.com>
2021-04-14 18:30:48 +0300

    Refactor structure project

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ViewGridAddIcon } from '@heroicons/react/solid'
+import {Avatar} from '../components/Avatar'
+
+export const Header = () => {
+  return (
+    <header className='flex justify-between items-center px-3.5 py-2 space-x-2'>
+      <img src='/images/logo.svg' className='w-8 h-8' />
+      <div className='flex items-center space-x-6'>
+        <ul className='flex space-x-4 text-gray-600'>
+          <li className='link'>Gmail</li>
+          <li className='link'>Images</li>
+        </ul>
+        <ViewGridAddIcon className='h-10 w-10 p-2 text-gray-600 cursor-pointer rounded-full hover:bg-gray-100' />
+        <Avatar />
+      </div>
+    </header>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,18 @@
 import Head from 'next/head'
-import { Avatar } from '../components/Avatar'
-import {
-  MicrophoneIcon,
-  SearchIcon,
-  ViewGridIcon,
-} from '@heroicons/react/solid'
+import { Header } from '../components/Header'
+import { Main } from '../components/Main'
 import { Footer } from '../components/Footer'
 export default function Home() {
-  const handleClick = (event) => {
-    event.preventDefault()
-  }
   return (
     <div className='flex flex-col h-screen'>
       <Head>
         <title>Google</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
       {/* HEADER */}
-      <header className='flex justify-between items-center px-3.5 py-2 space-x-2'>
-        <img src='/images/logo.svg' className='w-8 h-8' />
-        <div className='flex items-center space-x-6'>
-          <ul className='flex space-x-4 text-gray-600'>
-            <li className='link'>Gmail</li>
-            <li className='link'>Images</li>
-          </ul>
-          <ViewGridIcon className='h-10 w-10 p-2 text-gray-600 cursor-pointer rounded-full hover:bg-gray-100' />
-          <Avatar />
-        </div>
-      </header>
+      <Header />
       {/* MAIN */}
-      <main className='flex justify-center items-center flex-grow px-3.5'>
-        <form className='flex items-center flex-col w-full'>
-          <img src='/images/search.svg' className='h-20 mb-4' />
-          <div className='flex items-center w-full px-5 py-3 mb-4 rounded-full max-w-md border border-gray-200 focus-within:shadow-input hover:shadow-input'>
-            <SearchIcon className='h-5 w-5 cursor-pointer text-gray-600' />
-            <input type='text' className='flex-grow focus:outline-none px-2' />
-            <MicrophoneIcon className='h-5 w-5 cursor-pointer text-gray-600' />
-          </div>
-          <div className='hidden space-x-4 sm:flex'>
-            <button className='btn' onClick={handleClick}>
-              Google Search
-            </button>
-            <button className='btn' onClick={handleClick}>
-              I'm feeling lucky
-            </button>
-          </div>
-        </form>
-      </main>
+      <Main />
       <Footer />
     </div>
   )
