176b7434298bc02a1bb6cff335d66bd43ae1b680
John <oleinikov.dev@gmail.com>
2021-06-28 22:39:50 +0300

    Fix search with cyrillic symbols

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,7 @@ import { SearchResults } from '../components/search-page/SearchResults'
 import { Pagination } from './../components/Pagination'
 export default function Search({ results }) {
   const router = useRouter()
-  const totalResults = +results.searchInformation.totalResults
+  const totalResults = +results.searchInformation?.totalResults
   const searchQuery = router.query.data
   return (
     <div className='px-3.5'>
@@ -27,7 +27,9 @@ export async function getServerSideProps(context) {
   const CONTEXT_KEY = process.env.CONTEXT_KEY
   const startIndex = context.query.start || '0'
   const data = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.data}&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${encodeURIComponent(
+      context.query.data
+    )}&start=${startIndex}`
   ).then((response) => response.json())
   return {
     props: {
